5d9b0558d3efcfb2a5013d2b31699198f05c63e7
Kladius2006 <killerk2549@gmail.com>
2026-08-13 18:25:17 +0700

    Import Image from react-native

--- ReactGallery/App.tsx
+++ ReactGallery/App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.Text}>
+        Gallery
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +19,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Text: {
+    fontSize: 30,
+  }
 });
